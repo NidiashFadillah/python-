0c5248ca762d052a7e9bd2dc3bee1153c005e901
NidiashFadillah <nidiashfadillah@gmail.com>
2025-09-29 22:06:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+nilai1 = int(input("Masukkan nilai pertama:"))
+nilai2 = int(input("Masukkan nilai kedua: "))
+operator_lebih_besar = nilai1 > nilai2
+print(nilai1, ">", nilai2, "adalah", operator_lebih_besar)
+operator_lebih_kecil = nilai1 < nilai2
+print(nilai1, "<", nilai2, "adalah", operator_lebih_kecil)
+operator_sama_dengan = nilai1 == nilai2
+print(nilai1, "==", nilai2, "adalah", operator_sama_dengan)
+operator_tidak_sama_dengan = nilai1 != nilai2
+print(nilai1, "!=", nilai2, "adalah", operator_tidak_sama_dengan)
