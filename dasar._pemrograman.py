40d51c9187a82fa39228f5fd481b8c8aa16d3e38
NidiashFadillah <nidiashfadillah@gmail.com>
2025-09-29 21:31:08 +0700

    Add files via upload

---
+++ dasar._pemrograman.py
@@ -0,0 +1,15 @@
+nilai1 = 10
+nilai2 = 8
+penjumlahan = nilai1 + nilai2
+print(nilai1, "+", nilai2,"=", penjumlahan)
+pengurangan = nilai1 - nilai2
+print(nilai1, "-", nilai2,"=", pengurangan)
+perkalian = nilai1 * nilai2
+print(nilai1, "*", nilai2, "=", perkalian)
+pembagian = nilai1 / nilai2
+print(nilai1, "/", nilai2,"=", pembagian)
+pemangkatan = nilai1 ** nilai2
+print(nilai1, "**", nilai2, "=", pemangkatan)
+pembagian_bulat = nilai1 // nilai2
+print(nilai1, "//", nilai2, "=", pembagian_bulat)
+
