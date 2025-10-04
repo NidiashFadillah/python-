9f9be8333f8d629867368caa79fc26ea74697908
NidiashFadillah <nidiashfadillah@gmail.com>
2025-10-04 14:08:31 +0700

    Add files via upload

---
+++ individu.py
@@ -0,0 +1,21 @@
+print("===========================")
+print("      Warung Kelontong     ")
+print("===========================")
+
+nama = input("masukkan nama pembeli: ")
+banyaknya = int(input("Banyaknya barang yang dibeli: "))
+nama_barang = input("Nama barang yang di beli: ")
+harga = int(input("Harga satuan: "))
+total = harga * banyaknya
+
+terigu = 100000
+gula = 5000
+pasta_makanan = 50000
+
+
+print("===========================")
+print(f"Nama pembeli : {nama}")
+print(f"Bayaknya barang : {banyaknya}")
+print(f"Nama barang : {nama_barang}")
+print(f"harga satuan barang : Rp. {harga}")
+print(f"Total seluruh barang : Rp. {total}")
